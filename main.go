eeacaf75e6d3b33a679afb69256b660f9e7f8cc7
lukeraphael <luke.r.chow@gmail.com>
2020-10-01 18:26:50 +0800

    add simple tests for blockmanager

---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/ticklepoke/CZ4031/blockmanager"
+
+	"github.com/ticklepoke/CZ4031/bptree"
+)
+
+func main() {
+
+	t := bptree.NewTree()
+
+	// Insert 1, 4, 7, 10, 17, 21, 31, 25, 19, 20, 28, 42
+	// lecture example
+
+	arr := [12]int{1, 4, 7, 10, 17, 21, 31, 25, 19, 20, 28, 42}
+
+	for _, num := range arr {
+		err := t.Insert(num, []byte("hello friend"+strconv.Itoa(num)))
+		if err != nil {
+			fmt.Printf("error: %s\n\n", err)
+		}
+	}
+
+	// t.PrintTree()
+
+	b := blockmanager.InitializeBlockManager(100)
+
+	// record deletion
+	b.InsertRecord("tt0848228", "7.7", "9987")
+	rec := b.InsertRecord("tt0848224", "7.2", "99890")
+	b.DisplayStatus(false)
+
+	b.DeleteRecord(rec)
+	b.DisplayStatus(false)
+
+	b.InsertRecord("tt081", "9", "98")
+	b.DisplayStatus(false)
+
+	// multiple blocks
+	// for i := 0; i < 101; i++ {
+	// 	b.InsertRecord("tt"+strconv.Itoa(i), "7.7", strconv.Itoa(i))
+	// }
+	// b.DisplayStatus(true)
+}
