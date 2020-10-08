ad68cf4fcc3d5bdd98ce5c55741dd547ae355b87
lukeraphael <luke.r.chow@gmail.com>
2020-10-09 00:27:13 +0800

    add lecture examples

--- main.go
+++ main.go
@@ -4,9 +4,6 @@ import (
 	"fmt"
 	"strconv"
 
-	"github.com/ticklepoke/CZ4031/blockmanager"
-	"github.com/ticklepoke/CZ4031/tsvparser"
-
 	"github.com/ticklepoke/CZ4031/bptree"
 )
 
@@ -14,10 +11,21 @@ func main() {
 
 	t := bptree.NewTree()
 
-	// Insert 1, 4, 7, 10, 17, 21, 31, 25, 19, 20, 28, 42
-	// lecture example
+	// Insertion example
+	// 1, 4, 7, 10, 17, 21, 31, 25, 19, 20, 28, 42
+	// 20  |
+	// 7 17  | 25  |
+	// 1 4 5  | 7 10  | 17 19  | 20 21  | 25 31  |
 
-	arr := [12]int{1, 4, 7, 10, 17, 21, 31, 25, 19, 20, 28, 42}
+	// Deletion example
+	// build btree with the following
+	// 1, 4, 7, 10, 17, 21, 31, 25, 19, 20, 5
+	// delete 5
+	// 20  |
+	// 7 17  | 25  |
+	// 1 4  | 7 10  | 17 19  | 20 21  | 25 31  |
+
+	arr := []int{1, 4, 7, 10, 17, 21, 31, 25, 19, 20, 28, 42}
 
 	for _, num := range arr {
 		err := t.Insert(num, []byte("hello friend"+strconv.Itoa(num)))
@@ -26,19 +34,21 @@ func main() {
 		}
 	}
 
-	// t.PrintTree()
+	// t.Delete(5)
 
-	b := blockmanager.InitializeBlockManager(100)
+	t.PrintTree()
+
+	// b := blockmanager.InitializeBlockManager(100)
 
 	// testing out tsv
-	rows := tsvparser.ParseTSV("data.tsv")
+	// rows := tsvparser.ParseTSV("data.tsv")
 
-	for i := 0; i < 10; i++ {
-		tconts, rating, votes := rows[i][0], rows[i][1], rows[i][2]
+	// for i := 0; i < 10; i++ {
+	// 	tconts, rating, votes := rows[i][0], rows[i][1], rows[i][2]
 
-		// TODO: insert record to bptree
-		b.InsertRecord(tconts, rating, votes)
-	}
+	// 	// TODO: insert record to bptree
+	// 	b.InsertRecord(tconts, rating, votes)
+	// }
 
-	b.DisplayStatus(true)
+	// b.DisplayStatus(true)
 }
